4c3d820b01ae40a1324a2a4c68e2b2e02be8d592
vaaniGO <vaanigoenka445@gmail.com>
2025-12-12 11:12:20 +0530

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/boop/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/results" element={<Results />} />
